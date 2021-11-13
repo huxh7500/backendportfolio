221bf0aae858c8c4dee22f8f4edef5bffec27189
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-11-13 14:56:27 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express = require("express")
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db.js') 
+
 
 const app = express()
+dotenv.config()
+connectDB()
 
 app.get('/',(req,res) => res.send('API Running'))
 
+app.use('/users', require('./routes/userRoutes.js'))
+app.use('/profile', require('./routes/profileRoutes.js'))
+app.use('/posts', require('./routes/postsRoutes.js'))
+app.use('/auth', require('./routes/authRoutes.js'))
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`server run on port ${PORT}`))
+app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  )
