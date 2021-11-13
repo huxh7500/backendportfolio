0ee6ad16fa8693f90fe1393d0ca286385fa76275
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-11-13 16:24:15 +0200

    Changes

--- server.js
+++ server.js
@@ -7,16 +7,28 @@ const app = express()
 dotenv.config()
 connectDB()
 
-app.get('/',(req,res) => res.send('API Running'))
+// Init Middleware
+app.use(express.json());
 
+// Define Routes
 app.use('/users', require('./routes/userRoutes.js'))
 app.use('/profile', require('./routes/profileRoutes.js'))
 app.use('/posts', require('./routes/postsRoutes.js'))
 app.use('/auth', require('./routes/authRoutes.js'))
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(
-    PORT,
+    PORT, () =>
     console.log(
       `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
